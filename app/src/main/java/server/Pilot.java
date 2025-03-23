71ed4d31278dd9a755b9652c3306f913a8520d47
peternaylor <peter@oldtiogafarm.com>
2025-03-23 17:27:18 -0400

    be born

--- app/src/main/java/server/Pilot.java
+++ app/src/main/java/server/Pilot.java
@@ -1,9 +1,19 @@
 package server;
 
+import java.sql.SQLException;
+
 public class Pilot {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Bank_DB mybank;
+		try {
+			mybank = new Bank_DB();
+			mybank.queryAndPrint();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
