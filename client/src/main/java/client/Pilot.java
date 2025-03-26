3fe42e38d98e3cf378d279613c450df245a807e4
peternaylor <peter@oldtiogafarm.com>
2025-03-26 12:08:27 -0400

    upgraded client UI

--- client/src/main/java/client/Pilot.java
+++ client/src/main/java/client/Pilot.java
@@ -5,9 +5,8 @@ public class Pilot {
 	public static void main(String[] args) {
 
 		Screen sc = Screen.getScreen();
-		NetCom nc = NetCom.getCom("127.0.0.1", 4445);
-		//sc.updateScreen();
-		nc.getConnection();
+		//NetCom nc = NetCom.getCom("127.0.0.1", 4445);
+		sc.updateScreen();
 
 	}
 
--- client/src/main/java/client/Screen.java
+++ client/src/main/java/client/Screen.java
@@ -3,6 +3,7 @@ package client;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 class Screen extends JFrame {
 	
@@ -20,12 +21,38 @@ class Screen extends JFrame {
 		
 		public void actionPerformed(ActionEvent e) {
 			
-			this.username = ((JTextField) comps[0]).getText().trim();
-			this.password = ((JTextField) comps[2]).getText().trim();
+			this.username = ((JTextField) comps[0]).getText();
+			this.password = new String(((JPasswordField) comps[2]).getPassword());
 			System.out.print(username + '\n' + password + '\n');
 			
 		}
 		
+	}
+
+	private class BKL implements KeyListener {
+
+		private final JPasswordField jpf;
+
+		private BKL(JPasswordField jpf) {
+
+			this.jpf = jpf;
+
+		}
+
+		public void keyPressed(KeyEvent key) {
+
+			if (jpf.getEchoChar() != '*') {
+				jpf.setText(null);
+				jpf.setEchoChar('*');
+				jpf.updateUI();
+			}
+
+		}
+
+		public void keyTyped(KeyEvent key) {}
+
+		public void keyReleased(KeyEvent key) {}
+
 	}
 	
 	private static final int STRUTHEIGHT = 25;
@@ -38,16 +65,22 @@ class Screen extends JFrame {
 		JPanel panel = new JPanel();
 		JPanel loginPanel = new JPanel();
 		JLabel title = new JLabel("PeterBank Basic Frontend");
-		JTextField userf = new JTextField("                                                   ");
-		JTextField passf = new JTextField("                                                   ");
+		JLabel imagel = new JLabel(new ImageIcon("src/resources/bankimage.jpg"));
+		JTextField userf = new JTextField("Username");
+		JPasswordField passf = new JPasswordField("Password");
 		JButton loginButton = new JButton("LOGIN");
+		EmptyBorder viewBorder = new EmptyBorder(0, 50, 50, 50);
 		
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		loginPanel.setLayout(new BoxLayout(loginPanel, BoxLayout.Y_AXIS));
 		title.setPreferredSize(new Dimension(300, 100));
 		title.setFont(libera);
 		title.setAlignmentX(CENTER_ALIGNMENT);
+		imagel.setAlignmentX(CENTER_ALIGNMENT);
+		loginButton.setAlignmentX(CENTER_ALIGNMENT);
 		loginButton.setFont(libera);
+		passf.setEchoChar((char) 0);
+		passf.addKeyListener(new BKL(passf));
 		loginPanel.add(userf);
 		loginPanel.add(Box.createVerticalStrut(STRUTHEIGHT));
 		loginPanel.add(passf);
@@ -55,7 +88,9 @@ class Screen extends JFrame {
 		loginButton.addActionListener(new BankListener(loginPanel));
 		loginPanel.add(loginButton);
 		loginPanel.setPreferredSize(new Dimension(200, 200));
+		loginPanel.setBorder(viewBorder);
 		panel.add(title);
+		panel.add(imagel);
 		this.setLayout(new BorderLayout());
 		this.add(panel, BorderLayout.NORTH);
 		this.add(loginPanel, BorderLayout.CENTER);
